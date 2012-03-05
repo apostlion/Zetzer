8a2020b41da6980042239bc5fa325d7b3c818a3e
Ivan Cherevko <i@cherevko.net>
2012-03-05 22:25:52 +0200

    Added a script to collect keyword counts in user profiles

---
+++ backfill/count_of_keywords_in_user_profiles.rb
@@ -0,0 +1,14 @@
+file = open("/Users/ivancherevko/Downloads/track1/user_key_word.txt")
+
+keywords = Hash.new(0)
+
+file.each_line do |line|
+	id, keys = line.split("\t")
+	keys = keys.split(';').map{|x| x.split(':')[0]}
+	keys.each do |key|
+		keywords[key] ||= 0
+		keywords[key] += 1
+	end
+end
+
+pp keywords.sort_by{|x| x[1]}.reverse
