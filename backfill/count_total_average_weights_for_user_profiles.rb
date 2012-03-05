bbc9685c2686f9b124d3c60f1561ac55931ce174
Ivan Cherevko <i@cherevko.net>
2012-03-05 22:58:01 +0200

    Results of CTA for weights

--- backfill/count_total_average_weights_for_user_profiles.rb
+++ backfill/count_total_average_weights_for_user_profiles.rb
@@ -6,8 +6,8 @@ users = []
 
 file.each_line do |line|
 	id, keys = line.split("\t")
-	keys = keys.split(';').map{|x| x.split(':')[1]}
-	users << [keys.length, keys.fold(0){|s,v| s+v}, keys.fold(0){|s,v| s+v}/keys.length]
+	keys = keys.split(';').map{|x| x.split(':')[1].to_f}
+	users << [keys.length, keys.inject(0){|s,v| s+v}, keys.inject(0){|s,v| s+v}/keys.length]
 end
 
 puts users.map{|x| x.join("\t")}.join("\n")
